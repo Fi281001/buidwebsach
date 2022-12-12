747d44f79bcb4e03acc451aaaffc820d3e63b605
Fi281001 <pctrungmagic@gmail.com>
2022-12-12 23:16:47 +0700

    push

---
+++ src/bo/sachbo.java
@@ -0,0 +1,60 @@
+package bo;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import bean.sachbean;
+import dao.sachdao;
+
+public class sachbo {
+	sachdao lsach = new sachdao();
+	ArrayList<sachbean> ds;
+	
+	public ArrayList<sachbean> getsach(){
+		ds = lsach.getsach();
+		
+		return ds;
+	}
+	
+	public ArrayList<sachbean> timKiemTheoLoai(ArrayList<sachbean> ds ,String maLoai) {
+		ArrayList<sachbean> dsSachCungMa = new ArrayList<sachbean>();
+		for (sachbean item: ds) {
+			if (item.getMaLoai().equals(maLoai)) {
+				dsSachCungMa.add(item);
+			}
+		}
+		
+		return dsSachCungMa;
+	}
+	
+	public ArrayList<sachbean> timKiemTheoTen(ArrayList<sachbean> ds ,String tenSach) {
+		ArrayList<sachbean> dsSachCungTen = new ArrayList<sachbean>();
+		for (sachbean item: ds) {
+			if (item.getTenSach().toLowerCase().trim().contains(tenSach.toLowerCase().trim())
+			 || item.getMaLoai().toLowerCase().trim().contains(tenSach.toLowerCase().trim())
+			 || item.getTacGia().toLowerCase().trim().contains(tenSach.toLowerCase().trim())
+			) {
+				dsSachCungTen.add(item);
+			}
+		}
+		
+		return dsSachCungTen;
+	}
+	public ArrayList<sachbean> getpage(ArrayList<sachbean> ds, int s, int e){
+		ArrayList<sachbean> dsPage = new ArrayList<sachbean>();
+		for(int i=s; i<=e;i++) {
+			dsPage.add(ds.get(i));
+		}
+		return dsPage;
+	}
+	
+	public void them_sach(sachbean s, String sotap, Date date) {
+		try {
+			lsach.them(s, sotap, date);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+	}
+}
